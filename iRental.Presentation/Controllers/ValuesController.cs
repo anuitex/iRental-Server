3abee5e8cb36fbb50b946b96d9d3581b12aa0ce7
B0ER <qwertygo85@gmail.com>
2019-10-09 12:59:13 +0300

    create basic modules

--- iRental.Presentation/Controllers/ValuesController.cs
+++ iRental.Presentation/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace iRental.Controllers
+namespace iRental.Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- iRental.Presentation/Program.cs
+++ iRental.Presentation/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace iRental
+namespace iRental.Presentation
 {
     public class Program
     {
